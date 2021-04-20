4586bcb8e3e3591fcbbc8246d50c011d044d2092
wencwcoder <1748994690@qq.com>
2021-04-20 22:27:19 +0800

    feat(plugin.js): 删除自定义容器，退回到原来的版本。

--- .vuepress/config/plugins.js
+++ .vuepress/config/plugins.js
@@ -1,7 +1,3 @@
-const CARD_LIST = "cardList";
-const CARD_IMG_LIST = "cardImgList";
-const renderCardList = require('./utils');
-
 module.exports = [
   ["@vuepress/last-updated", {
     transformer: (timestamp, lang) => {
@@ -28,25 +24,5 @@ module.exports = [
     size: 2,
     shape: "star", // ['star' | 'circle']
     zIndex: 999999999
-  }],
-  ["container", {
-    // 卡片列表
-    type: CARD_LIST,
-    render: (tokens, idx) => {
-      // tokens 是整个 md 文件的虚拟 dom 结构数组
-      // idx 是 tokens 中':::' 所在的索引，而且是当前指定type的 ':::'，分别有开始和结束两次的 idx
-      // if (tokens[idx].nesting === 1) { // 开头的 ':::' 标记
-      // } else { // 结束的 ':::' 标记
-      // }
-      // 注意：修改这里面的代码后需要在 md 文件保存一下才会重新执行渲染
-      return renderCardList(tokens, idx, CARD_LIST);
-    }
-  }],
-  ["container", {
-    // 图文卡片列表
-    type: CARD_IMG_LIST,
-    render: (tokens, idx) => {
-      return renderCardList(tokens, idx, CARD_IMG_LIST);
-    }
   }]
 ]
--- blogs/frontEnd/README.md
+++ blogs/frontEnd/README.md
@@ -8,16 +8,3 @@ tags:
 ---
 
 <!-- more -->
-
-::: cardImgList 2
-```yaml
-- img: /img/common/avatar.png
-  link: /znote/other/friends.html
-  name: For you
-  desc: 你若盛开，清风自来。
-- img: /img/common/avatar.png
-  link: /znote/other/friends.html
-  name: To me
-  desc: 心若浮沉，浅笑安然。
-```
-:::
